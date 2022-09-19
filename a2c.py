41dc2f94e14cafcccac19bc1c690fa8807206c81
Volodymyr Tsumanchuk <volodymyr.tsumanchuk@splynx.com>
2022-09-19 10:57:31 +0200

    Split learning and runing scripts

--- a2c.py
+++ a2c.py
@@ -1,34 +1,11 @@
 import gym
 import gym_examples
-import pygame
 from stable_baselines3 import A2C
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.env_checker import check_env
 
 # Parallel environments
 env_name = "gym_examples/MySnake-v0"
-# env = gym.make(env_name)
-env = gym.make(env_name, render_mode="none")#rgb_array
-
-#env = make_vec_env(env_name, n_envs=1)
-
-# check_env(env)
-# print('Env okay')
-# exit()
-
-#model = A2C("MultiInputPolicy", env, verbose=1)
-model = A2C.load("a2c_snake1", env=env)
-
-print('before learn')
-#model.learn(total_timesteps=500000)
-
-print('Save model')
-model.save("a2c_snake1")
-
-del model # remove to demonstrate saving and loading
-del env
-
 env = gym.make(env_name, render_mode="human")
+
 model = A2C.load("a2c_snake1",env=env)
 
 obs = env.reset()
